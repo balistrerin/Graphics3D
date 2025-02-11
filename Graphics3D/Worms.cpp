cc7edede5262efc93cda44a7f54db72f3488ef85
Nick Balistreri <balistrerin@gmail.com>
2025-02-11 10:02:36 -0800

    Updates

--- Graphics3D/Worms.cpp
+++ Graphics3D/Worms.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <optional>
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
@@ -13,7 +15,7 @@ public:
 	olc::vf2d vTarget = { 400.0f, 300.0f };
 
 	float fMuzzleVelocity = 40.0f;
-	float vGravity = 40.f;
+	float fGravity = 4.0f;
 
 
 public:
@@ -23,22 +25,77 @@ public:
 		return true;
 	}
 
+	std::optional<std::pair<float, float>> CalculateTrajectory(
+		const olc::vf2d& source,
+		const olc::vf2d& target,
+		const float gravity,
+		const float muzzle
+	)
+	{
+		olc::vf2d D = target - source;
+		float u = muzzle;
+		float G = gravity;
+
+		float a = (0.5f * G * (D.x / u) * (D.x / u));
+		float b = D.x;
+		float c = (0.5f * G * (D.x / u) * (D.x / u)) - D.y;
+
+		float d = b * b - 4.0f * a * c;
+
+		if (d < 0)
+		{
+			return std::nullopt;
+		}
+
+		float angle1 = atan((-b + sqrt(d)) / (2.0f * a));
+		float angle2 = atan((-b - sqrt(d)) / (2.0f * a));
+
+		float dir = (D.x >= 0 ? 0.0f : 3.14159f);
+
+		return std::pair{ dir + angle1, dir + angle2 };
+	}
+
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		if (GetMouse(0).bHeld)
 		{
-			vSource.x = GetMousePos().x;
+			vSource = GetMousePos();
 		}
 
 		if (GetMouse(1).bHeld)
 		{
-			vTarget.x = GetMousePos().x;
+			vTarget = GetMousePos();
 		}
 
 		Clear(olc::VERY_DARK_MAGENTA);
 
 		FillCircle(vSource, 10, olc::CYAN);
 		FillCircle(vTarget, 10, olc::YELLOW);
+
+		olc::vf2d vLine = (vTarget - vSource).norm();
+		DrawLine(vSource - vLine * 2000.0f, vSource + vLine * 2000.0f, olc::MAGENTA, 0xf0f0f0f0);
+
+		auto angles = CalculateTrajectory(vSource, vTarget, fGravity, fMuzzleVelocity);
+
+		if (!angles.has_value())
+			return true;
+
+		olc::vf2d p1_vel = olc::vd2d(fMuzzleVelocity, angles->first).cart();
+		olc::vf2d p1_pos = vSource;
+		olc::vf2d p2_vel = olc::vd2d(fMuzzleVelocity, angles->second).cart();
+		olc::vf2d p2_pos = vSource;
+
+		olc::vf2d vGravity = { 0.0f, fGravity };
+
+		float dt = 0.1f;
+		for (int n = 0; n < 1000; n++)
+		{
+			p1_pos = p1_vel * (n * dt) + 0.5f * vGravity * (n * dt) * (n * dt);
+			p2_pos = p2_vel * (n * dt) + 0.5f * vGravity * (n * dt) * (n * dt);
+
+			Draw(p1_pos + vSource, olc::GREEN);
+			Draw(p2_pos + vSource, olc::WHITE);
+		}
 
 		return true;
 	}
